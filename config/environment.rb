39e9de75b4db59f631b29dd66c54b7b0146b0e17
Inigo-santiu <dstavroff@gmail.com>
2020-08-10 13:44:58 +0200

    lol

--- config/environment.rb
+++ config/environment.rb
@@ -5,12 +5,12 @@ require_relative 'application'
 Rails.application.initialize!
 
 ActionMailer::Base.smtp_settings = {
-  :user_name => ENV[SENDGRID_LOGIN],
-  :password => ENV[SENDGRID_PWD],
+  :user_name => ENV['SENDGRID_LOGIN'],
+  :password => ENV['SENDGRID_PWD'],
   :domain =>  'mysite.ca',
   :address => 'smtp.sendgrid.net',
   :port => 587,
-  :authentification => plain,
+  :authentication => :plain,
   :enable_starttls_auto => true
 }
 
